6ddfc2699c562a41d323dd6fa43ce75c6d44f910
Barlus Cuda <barluscuda@gmail.com>
2025-01-28 17:42:25 +0700

    add navbar reponsive

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { FaFacebook, FaGithub } from "react-icons/fa"
+import { FaArrowLeft, FaFacebook, FaGithub } from "react-icons/fa"
 import { IoSearch } from "react-icons/io5"
 
 const Navbar = () => {
@@ -17,11 +17,15 @@ const Navbar = () => {
                     <h1 className="netron-font text-gray-300 text-xl">Barlus Page</h1>
                 </ul>
                 <ul className="flex items-center gap-5 text-gray-300">
-                    <input type="text" className="hidden md:block w-96 h-10 bg-gray-800 rounded-md outline-none p-4" placeholder="Search" />
+                    <input type="search" className="hidden md:block w-96 h-10 bg-gray-800 rounded-md outline-none p-4" placeholder="Search" />
                     <li><IoSearch className="md:hidden w-7 h-7 cursor-pointer" onClick={toggleSearchBox} /></li>
                     <li><FaFacebook className="w-6 h-6 cursor-pointer" /></li>
                     <li><FaGithub className="w-6 h-6 cursor-pointer" /></li>
                 </ul>
+            </div>
+            <div className={`${searchBox ? "fixed" : "hidden"} md:hidden top-0 w-full h-16 max-w-screen-md flex justify-between items-center bg-gray-900 gap-4 p-2 px-4`}>
+                <FaArrowLeft className="w-5 h-5 cursor-pointer"  onClick={toggleSearchBox} />
+                <input type="search" className="w-full h-10 bg-gray-800 rounded-md outline-none p-4" placeholder="Search" />
             </div>
         </nav>
     )
